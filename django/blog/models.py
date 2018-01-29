5bc9ef493ec5cb3e6dbab0c5b139082c91ae86f4
seokjae hong <hsj2334@gmail.com>
2018-01-29 16:42:15 +0900

    blog app을 INSTALLED_APPS에 추가 , migration 생성 및 admin에 등록

--- django/blog/models.py
+++ django/blog/models.py
@@ -1,11 +1,16 @@
-from datetime import timezone
+
 
 from django.db import models
 
 # Create your models here.
+from django.utils import timezone
+
 
 class Post(models.Model):
-    author = models.ForeignKey('auth.User')
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+    )
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
     created_date = models.DateTimeField(
