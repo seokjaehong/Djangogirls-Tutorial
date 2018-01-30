f5a86c9feba5c0ffc63217ace4dc83234f0edfbd
seokjae hong <hsj2334@gmail.com>
2018-01-30 12:04:15 +0900

    template/blog/post_list.html작성,post_list리턴

--- django/blog/views.py
+++ django/blog/views.py
@@ -12,8 +12,11 @@ def post_list(reuqest):
     # 5. config.urls모듈은 ''(admin/를 제외한 모든요청)을 blog.urls모듈로 전달
     # 6. blog.urls모듈은 받은 요청의 url과 일치하는 패턴이 있는지 검사
     # 7. 있다면 일치하는 패턴과 연결된 함수(view)를 실행
+    #   7.1 setting모듈의 Templates 속성 내의 목록에서 blog/post_list.html파일의 내용을 가져옴
+    #   7.2 가져온 내용을 처리 (렌더렝, render())하여 리턴
     # 8. 함수의 실행결과(리턴값)을 브라우저로 다시전달
     # Http프로토콜로 텍스트 데이터 응답을 반환
     # return HttpResponse('Post List')
-    return HttpResponse('<html><body><h1>Post List</h1><p>post 목록을 보여줄 예정입니다.</p></body></html>')
+    # return HttpResponse('<html><body><h1>Post List</h1><p>post 목록을 보여줄 예정입니다.</p></body></html>')
+    return render(reuqest, 'blog/post_list.html')
 
