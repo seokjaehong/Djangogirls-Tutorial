a3e0031ba88751ca65553d3d02ac9226303a9fcb
seokjae hong <hsj2334@gmail.com>
2018-01-30 12:03:41 +0900

    template폴더 추가 및 setting의 template옵션의 dir 항목에 해당폴더 경로 추가

--- django/config/settings.py
+++ django/config/settings.py
@@ -13,8 +13,9 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# ************************BASE_DIR는 'Django'폴더를 뜻함****************************
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+TEMPLATES_DIR = os.path.join(BASE_DIR,)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -57,7 +58,10 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        #Django에서 템플릿을 검색할 때 사용할 경로 목록
+        'DIRS': [
+            TEMPLATES_DIR
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
