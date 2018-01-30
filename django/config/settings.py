636076018e7041e3ab687f937b4f5480581bc51d
seokjae hong <hsj2334@gmail.com>
2018-01-30 12:15:36 +0900

    setting.py수정

--- django/config/settings.py
+++ django/config/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 # ************************BASE_DIR는 'Django'폴더를 뜻함****************************
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TEMPLATES_DIR = os.path.join(BASE_DIR,)
+TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
